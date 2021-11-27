308a92ba7d75fdb0618a2a8ae20e378cf3ca443c
Jeferson Veiga <jeferson.veiiga@icloud.com>
2021-11-26 22:54:49 -0300

    Delete project-expense-tracker directory

--- project-expense-tracker/pages/register/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Register } from '../../src/pages/Register';
-
-const index = () => {
-    return <Register />;
-};
-
-export default index
-
--- project-expense-tracker/src/components/organism/ImageSideBar/index.tsx
+++
@@ -1,7 +0,0 @@
-export const ImageSideBar = () => {
-    return (
-        <aside className="flex">
-            <img src="/hero-img.svg" alt="image" />
-        </aside>
-    );
-};
